3657a326e73d58893f8ea83f808aa1b7ff21df99
Kazakov Alexander <kazakov_ai87@mail.ru>
2024-11-24 18:11:22 +0300

    update

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -19,103 +19,105 @@ let DB = [];
 const notificationInterval = 5 * 60 * 60; // 5 hours
 let lastMessageTime = new Date('31 Dec 2050').getTime() / 1000; // init
 
-
-/* BOT COMMANDS */
-bot.onText(/\/здароваденис/, (msg) => {
-    const text = randomArray(greetings);
-    bot.sendMessage(msg?.chat?.id, text);
-});
-
-bot.onText(/\/дениспосчитай/, (msg) => {
-    const message = getLineUp(msg, DB);
-    bot.sendMessage(msg?.chat?.id, message, {parse_mode: 'HTML'});
-});
-
-bot.onText(/\/денисподели/, (msg) => {
-    const message = getSquads(msg, DB);
-    if (message) {
-        bot.sendMessage(msg?.chat?.id, message, {parse_mode: 'HTML'});
-    }
-});
-
-bot.onText(/\/денисподскажи/, (msg) => {
-    const text = randomArray(answers);
-    bot.sendMessage(msg?.chat?.id, text);
-});
-
-bot.onText(/\/галяотмена/, () => {
-    [DB, lastMessageTime] = clearDB();
-    const video = randomArray(cancelGifs);
-    bot.sendDocument(CHAT_ID, video);
-});
-
-/* ON USER INPUT */
-bot.onText(/\+/, (msg) => {
-    const chatId = msg?.chat?.id;
-    const msgId = msg?.message_id;
-    const reaction = setReaction(likeEmoji);
-
-    bot.getUpdates().then(res => {
-        res.map(mes => {
-            const user = mes?.message?.from;
-            user.message = mes?.message?.text;
-            DB.push(user);
-
-            const playersCount = DB.length;
-            if (playersCount) {
-                bot.sendMessage(chatId, String(playersCount));
-            }
-        });
+exports.handler = async () => {
+    /* BOT COMMANDS */
+    bot.onText(/\/здароваденис/, (msg) => {
+        const text = randomArray(greetings);
+        bot.sendMessage(msg?.chat?.id, text);
     });
 
-    bot.setMessageReaction(chatId, msgId, {reaction: reaction});
-});
+    bot.onText(/\/дениспосчитай/, (msg) => {
+        const message = getLineUp(msg, DB);
+        bot.sendMessage(msg?.chat?.id, message, {parse_mode: 'HTML'});
+    });
 
-bot.onText(/^-+$/gm, (msg) => {
-    try {
+    bot.onText(/\/денисподели/, (msg) => {
+        const message = getSquads(msg, DB);
+        if (message) {
+            bot.sendMessage(msg?.chat?.id, message, {parse_mode: 'HTML'});
+        }
+    });
+
+    bot.onText(/\/денисподскажи/, (msg) => {
+        const text = randomArray(answers);
+        bot.sendMessage(msg?.chat?.id, text);
+    });
+
+    bot.onText(/\/галяотмена/, () => {
+        [DB, lastMessageTime] = clearDB();
+        const video = randomArray(cancelGifs);
+        bot.sendDocument(CHAT_ID, video);
+    });
+
+    /* ON USER INPUT */
+    bot.onText(/\+/, (msg) => {
         const chatId = msg?.chat?.id;
         const msgId = msg?.message_id;
-        const reaction = setReaction(dislikeEmoji);
-    
+        const reaction = setReaction(likeEmoji);
+
         bot.getUpdates().then(res => {
             res.map(mes => {
-                const userId = mes?.message?.from.id;
-                const index = findLastIndex(DB,user => user.id === userId);
-                if (index > -1) {
-                    DB.splice(index, 1);
-    
-                    const playersCount = DB.length;
-                    if (playersCount) {
-                        bot.sendMessage(chatId, String(playersCount));
-                    }
+                const user = mes?.message?.from;
+                user.message = mes?.message?.text;
+                DB.push(user);
+
+                const playersCount = DB.length;
+                if (playersCount) {
+                    bot.sendMessage(chatId, String(playersCount));
                 }
             });
         });
-    
+
         bot.setMessageReaction(chatId, msgId, {reaction: reaction});
-    } catch (error) {
-        console.log(error);
-    }
-});
+    });
 
-bot.on('message', () => {
-    lastMessageTime = Math.floor(Date.now() / 1000);
-});
+    bot.onText(/^-+$/gm, (msg) => {
+        try {
+            const chatId = msg?.chat?.id;
+            const msgId = msg?.message_id;
+            const reaction = setReaction(dislikeEmoji);
+        
+            bot.getUpdates().then(res => {
+                res.map(mes => {
+                    const userId = mes?.message?.from.id;
+                    const index = findLastIndex(DB,user => user.id === userId);
+                    if (index > -1) {
+                        DB.splice(index, 1);
+        
+                        const playersCount = DB.length;
+                        if (playersCount) {
+                            bot.sendMessage(chatId, String(playersCount));
+                        }
+                    }
+                });
+            });
+        
+            bot.setMessageReaction(chatId, msgId, {reaction: reaction});
+        } catch (error) {
+            console.log(error);
+        }
+    });
 
-/* SCHEDULE */
-const scheduledMessage = new schedule.scheduleJob('0 59 7 * * 1,5', function () {
-    const {scheduledText, video} = scheduled;
-    bot.sendDocument(CHAT_ID, video,{caption: scheduledText, parse_mode: 'HTML'});
-});
+    bot.on('message', () => {
+        lastMessageTime = Math.floor(Date.now() / 1000);
+    });
 
-const notificationMessage = new schedule.scheduleJob('0 0 19 * * 1,5', function () {
-    const {notification} = scheduled;
-    const currentTime = Math.floor(Date.now() / 1000);
-    if (currentTime - lastMessageTime > notificationInterval) {
-        bot.sendMessage(CHAT_ID, notification);
-    }
-});
+    /* SCHEDULE */
+    const scheduledMessage = new schedule.scheduleJob('0 59 7 * * 1,5', function () {
+        const {scheduledText, video} = scheduled;
+        bot.sendDocument(CHAT_ID, video,{caption: scheduledText, parse_mode: 'HTML'});
+    });
 
-const clearDBJob = new schedule.scheduleJob('0 0 21 * * 1,5', function () {
-    [DB, lastMessageTime] = clearDB();
-});
+    const notificationMessage = new schedule.scheduleJob('0 0 19 * * 1,5', function () {
+        const {notification} = scheduled;
+        const currentTime = Math.floor(Date.now() / 1000);
+        if (currentTime - lastMessageTime > notificationInterval) {
+            bot.sendMessage(CHAT_ID, notification);
+        }
+    });
+
+    const clearDBJob = new schedule.scheduleJob('0 0 21 * * 1,5', function () {
+        [DB, lastMessageTime] = clearDB();
+    });
+    return { statusCode: 200 };
+};
