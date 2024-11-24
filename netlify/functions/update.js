9c30c248df7a0afb2f26acff32c85604c5b20526
Kazakov Alexander <kazakov_ai87@mail.ru>
2024-11-24 19:51:02 +0300

    update

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -12,9 +12,12 @@ const answers = require('../../src/const/answers');
 const greetings = require('../../src/const/greetings');
 
 // const
+const url = 'https://tg-bot-node.netlify.app/.netlify/functions/update';
 const TOKEN = process.env.TOKEN;
 const CHAT_ID = process.env.CHAT_TEST_ID;
 const bot = new TelegramBot(TOKEN);
+bot.setWebHook(`${url}/bot${TOKEN}`);
+
 let DB = [];
 const notificationInterval = 5 * 60 * 60; // 5 hours
 let lastMessageTime = new Date('31 Dec 2050').getTime() / 1000; // init
